d5586c91d60a75f97b96b431cf8098104f57e8f4
darioandrade@gmail.com <darioandrade@gmail.com@49075b40-1e26-6008-5174-a2fb3ac9b867>
2011-05-28 18:42:26 +0000

    gerador de grafos completo, com opcao de gerar um unico grafo, com opcoes de debug (0, 1, 2)  debug 0: nenhuma saida no stderr  debug 1: imprime o que esta fazendo e os parametros utilizados em stderr ou  debug 2: imprime vertice por vertice o numero aleatorio gerado e a insercao (ou nao) da aresta correspondente aos outros vertices
     se nao chamar com parametros gera as 20 sequencias, 10 de cada tipo com debug = 1 (imprime o que esta fazendo em stderr)

---
+++ trunk/aa-trabalho1-20111/GeraGrafo.cpp
@@ -0,0 +1,81 @@
+/*
+ * GeraGrafo.cpp
+ *
+ *  Created on: May 28, 2011
+ *      Author: darioandrade
+ */
+
+#include <stdlib.h>
+#include <math.h>
+
+#include "AdjacencyList.h"
+
+#define P_TIPO_0 0.2
+
+double randd ( )
+{
+    return ( ( double ) rand( ) ) / RAND_MAX;
+}
+
+void GeraGrafo ( FILE* f, int nVertex, int type, int debug )
+{
+    // alimentando a semente randomica
+    srand( time( NULL ) );
+
+    // calculando p
+    double p = ( type == 0 ) ? P_TIPO_0 : ( 1.0 / ( 2.0 * sqrt( nVertex ) ) );
+
+    if ( debug > 0 )
+    {
+        fprintf( stderr,
+                "Gerando %d vertices de grafo nao direcionado tipo %d (p = %.7f)\n",
+                nVertex, type, p );
+    }
+
+    // criando o grafo
+    AdjacencyList graph( nVertex );
+
+    // percorrendo do primeiro vertice ate o penultimo (zero based list)
+    for ( int i = 0; i < nVertex - 1; i++ )
+    {
+        if ( debug > 1 )
+        {
+            fprintf( stderr, "  vertice %d: ", i );
+        }
+
+        for ( int j = i + 1; j < nVertex; j++ )
+        {
+            double x = randd( );
+
+            if ( debug > 1 )
+            {
+                fprintf( stderr, " %.4f", x );
+            }
+
+            // should we cast an edge to j?
+            if ( x < p )
+            {
+                if ( debug > 1 )
+                {
+                    fprintf( stderr, "Y" );
+                }
+
+                graph.addEdge( i, j );
+            }
+            else
+            {
+                if ( debug > 1 )
+                {
+                    fprintf( stderr, " " );
+                }
+            }
+        }
+
+        if ( debug > 1 )
+        {
+            fprintf( stderr, "\n" );
+        }
+    }
+
+    graph.write( f );
+}
